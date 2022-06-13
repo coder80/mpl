5f1f406ffe5a9b07a71b18e723bcdaadbea4c91e
Andrey Morozov <andrey.morozov@intel.com>
2022-06-14 00:31:21 +0300

    Init repo

---
+++ demuxers/ffdemuxer/FFDemuxer.h
@@ -0,0 +1,16 @@
+extern "C" {
+    #include "libavformat/avformat.h"
+    #include "libavformat/avio.h"
+    #include "libavcodec/avcodec.h"
+    #include "libavutil/audio_fifo.h"
+    #include "libavutil/avassert.h"
+    #include "libavutil/avstring.h"
+    #include "libavutil/frame.h"
+    #include "libavutil/opt.h"
+    #include "libswresample/swresample.h"
+}
+
+class FFDemuxer {
+public:
+    FFDemuxer();
+};
